f231d9a8e83fd97467734be1626424b47aa0a191
Seanforfun <32276204+Seanforfun@users.noreply.github.com>
2018-03-17 20:56:27 -0400

    [Function add]
    1.Shell's Sort.
    2.MergeSort.

--- Algorithm(4th_Edition)/src/ca/mcmaster/chapter/two/Sort/Sort.java
+++ Algorithm(4th_Edition)/src/ca/mcmaster/chapter/two/Sort/Sort.java
@@ -1,6 +1,7 @@
 package ca.mcmaster.chapter.two.Sort;
 
 public class Sort {
+	private static Comparable[] aux;
 	@SuppressWarnings("rawtypes")
 	public static void selectionSort(Comparable[] a){
 		int length = a.length;
@@ -19,6 +20,33 @@ public class Sort {
 				swap(a, j, j-1);
 		}
 	}
+	
+	public static void ShellSort(Comparable[] a){
+		int length = a.length;
+		int h = 1;
+		while(h < length/3)	h = 3*h + 1;
+		while(h >= 1){
+			for(int i = h; i < length; i++){
+				for(int j = i; j >= h && less(a[j], a[j-h]); j -= h)
+					swap(a, j, j-h);
+			}
+			h /= 3;
+		}
+	}
+	
+	public static void mergeSort(Comparable[] a){
+		aux = new Comparable[a.length];
+		mergeSortIn(a, 0, a.length - 1);
+	}
+	
+	public static void mergeSortIn(Comparable[] a, int lo, int hi){
+		if(lo >= hi) return;
+		int mid = (hi - lo) / 2 + lo;
+		mergeSortIn(a, lo, mid);
+		mergeSortIn(a, mid + 1, hi);
+		merge(a, lo, mid, hi);
+	}
+	
 	public static Boolean less(Comparable a, Comparable b){
 		return a.compareTo(b) < 0;
 	}
@@ -38,10 +66,26 @@ public class Sort {
 			if(less(a[i], a[i-1])) return false;
 		return true;
 	}
+	public static void merge(Comparable[] a, int lo, int mid, int hi){
+		int i = lo, j = mid + 1;
+		for (int k = lo; k < aux.length; k++) 
+			aux[k] = a[k];
+		for(int k = lo; k < a.length; k++){
+			if(i > mid)								a[k] = aux[j++];
+			else if(j > hi) 							a[k] = aux[i++];
+			else if(less(aux[i], aux[j]))	a[k] = aux[i++];
+			else											a[k] = aux[j++];
+		}
+	}
+	
 	public static void main(String[] args) {
 		Integer[] a = new Integer[]{2,3,5,2,45,1,4,2,5,7,3,7,432,96,7,23,8};
+//		Integer[] a = new Integer[]{1,3,5,9,2,5,6,7};
 //		selectionSort(a);
-		insertSort(a);
+//		insertSort(a);
+//		ShellSort(a);
+//		merge(a, 0, 3, 7);
+		mergeSort(a);
 		show(a);
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# LeetCodeSolutions
-Personal solution, Java
+# Algorithm Learning
+## Algorithm(4th_Edition)
+Reading notes of book Algorithm(4th Algorithm),ISBN: 9787115293800.
+All java realization codes are placed in different packages.
