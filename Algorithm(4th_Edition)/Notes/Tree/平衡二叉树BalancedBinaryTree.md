0b371fea1a6ec2740e91836b9aee27015342c4bd
Seanforfun <32276204+Seanforfun@users.noreply.github.com>
2018-05-19 20:53:28 -0400

    [Bug fix]
    1.Fix the bug in LL,RR
    2.Add conclusion and code for insert.

--- Algorithm(4th_Edition)/Notes/Tree/平衡二叉树BalancedBinaryTree.md
+++ Algorithm(4th_Edition)/Notes/Tree/平衡二叉树BalancedBinaryTree.md
@@ -16,32 +16,127 @@
 >高度是指当前结点到叶子结点的最长路径，如所有叶子结点的高度都为0。
 ![height](https://i.imgur.com/dA8OWAs.png)
 
+## 旋转
 * 左左单旋转(LL)情景①分析
+>左左指的是在结点的左结点的左结点加入元素，被称为LL，此时要通过向右旋转使树平衡。
+
 ![LL](https://i.imgur.com/kpMVlUs.png)
 ```Java
-	private AVLNode singleRotateLeft(AVLNode n){
+	/**
+	 * @Description: LL插入造成不平衡，通过单次向右旋转重新平衡树。
+	 * @param n
+	 * @return
+	 */
+	private AVLNode singleRotateRight(AVLNode n){
 		AVLNode left1 = n.left;
 		n.left = left1.right;
 		left1.right = n;
-		left1.height = Math.max(left1.left.height, left1.right.height) + 1;
-		n.height = Math.max(n.left.height, n.right.height) + 1;
+		n.height = Math.max(height(n.left), height(n.right)) + 1;
+		left1.height = Math.max(height(left1.left), n.height) + 1;
 		return left1;
 	}
 ```
 
 * 右右单旋转(RR)情景④分析
+>右右指的是在结点的右结点的右结点插入元素造成的不平衡，成为RR，此时需要通过向左旋转使树平衡。
 ![RR](https://i.imgur.com/CkFlj4X.png)
 ```Java
-	private AVLNode singleRotateRight(AVLNode n){
+	/**
+	 * @Description: RR插入造成不平衡，通过单次向左旋转重新平衡树。
+	 * @param n
+	 * @return
+	 */
+	private AVLNode singleRotateLeft(AVLNode n){
 		AVLNode right1 = n.right;
-		n.left = right1.left;
+		n.right = right1.left;
 		right1.left = n;
-		right1.height = Math.max(right1.left.height, right1.right.height) + 1;
-		n.height = Math.max(n.left.height, n.right.height) + 1;
+		n.height = Math.max(height(n.left), height(n.right)) + 1;
+		right1.height = Math.max(n.height, height(right1.right)) + 1;
 		return right1;
 	}
 ```
 
 * 平衡二叉树的双旋转算法与实现
 * 左右双旋转(LR)情景②分析
+>在左子树的右子树插入造成的不平衡，LR，需要两次旋转实现平衡。
 ![LR](https://i.imgur.com/926XxBS.png)
+```Java
+	/**
+	 * @Description: LR造成的不平衡，需要两次旋转。
+	 * @param n
+	 * @return
+	 */
+	private AVLNode doubleRotateLR(AVLNode n){
+		n.left = singleRotateLeft(n.left);
+		return singleRotateRight(n);
+	}
+```
+
+* 右左双旋转(RL)情景③分析
+>向右结点的左结点插入新的元素，RL，两次旋转实现平衡。
+![RL](https://i.imgur.com/oZwL15T.png)
+```Java
+	/**
+	 * @Description: RL造成的不平衡，两次旋转。
+	 * @param n
+	 * @return
+	 */
+	private AVLNode doubleRotateRL(AVLNode n){
+		n.right = singleRotateRight(n.right);
+		return singleRotateLeft(n);
+	}
+```
+
+* 插入结点
+```Java
+	public void insert(V v){
+		if(null == v){
+			throw new RuntimeException("Cannot insert null to AVLTree");
+		}
+		root = insert(v, root);
+	}
+
+	/**
+	 * @Description: 向AVLTree中加入元素。
+	 * @param v
+	 */
+	private AVLNode insert(V v, AVLNode node){
+		//当前节点已经为空，新建结点。
+		if(null == node){
+			node = new AVLNode(v);
+		}else if (v.compareTo(node.v) < 0) {
+			//L
+			node.left = insert(v, node.left);
+			//如果造成了不平衡
+			if(height(node.left) - height(node.right) == 2){
+				//判断是LL还是LR
+				if(v.compareTo(node.left.v) < 0){
+					//LL
+					node = singleRotateRight(node);
+				}else{
+					//LR
+					node = doubleRotateLR(node);
+				}
+			}
+		}else if(v.compareTo(node.v) > 0){
+			//R
+			node.right = insert(v, node.right);
+			//判断是否需要旋转
+			if(height(node.right) - height(node.left) == 2){
+				//判断LR/RR
+				if(v.compareTo(node.right.v) > 0){
+					//RR
+					node = singleRotateLeft(node);
+				}else{
+					//RL
+					node = doubleRotateRL(node);
+				}
+			}
+		}else{
+			//如果要插入的数据和当前遍历到数据一致，do nothing
+			;
+		}
+		node.height = Math.max(height(node.left), height(node.right)) + 1;
+		return node;
+	}
+```
--- Algorithm(4th_Edition)/src/ca/mcmaster/chapter/three/bitree/BalancedBinaryTree.java
+++ Algorithm(4th_Edition)/src/ca/mcmaster/chapter/three/bitree/BalancedBinaryTree.java
@@ -1,6 +1,7 @@
 package ca.mcmaster.chapter.three.bitree;
 
 public class BalancedBinaryTree<V extends Comparable<V>> {
+	private AVLNode root;
 	private class AVLNode{
 		V v;
 		int height;	//当前节点的子树的高度
@@ -19,24 +20,136 @@ public class BalancedBinaryTree<V extends Comparable<V>> {
 		}
 	}
 	private int height(AVLNode n){
+		if(n == null)		return -1;
 		return n.height;
 	}
 	
-	private AVLNode singleRotateLeft(AVLNode n){
+	/**
+	 * @Description: LL插入造成不平衡，通过单次向右旋转重新平衡树。
+	 * @param n
+	 * @return
+	 */
+	private AVLNode singleRotateRight(AVLNode n){
 		AVLNode left1 = n.left;
 		n.left = left1.right;
 		left1.right = n;
-		left1.height = Math.max(left1.left.height, left1.right.height) + 1;
-		n.height = Math.max(n.left.height, n.right.height) + 1;
+		n.height = Math.max(height(n.left), height(n.right)) + 1;
+		left1.height = Math.max(height(left1.left), n.height) + 1;
 		return left1;
 	}
 	
-	private AVLNode singleRotateRight(AVLNode n){
+	/**
+	 * @Description: RR插入造成不平衡，通过单次向左旋转重新平衡树。
+	 * @param n
+	 * @return
+	 */
+	private AVLNode singleRotateLeft(AVLNode n){
 		AVLNode right1 = n.right;
-		n.left = right1.left;
+		n.right = right1.left;
 		right1.left = n;
-		right1.height = Math.max(right1.left.height, right1.right.height) + 1;
-		n.height = Math.max(n.left.height, n.right.height) + 1;
+		n.height = Math.max(height(n.left), height(n.right)) + 1;
+		right1.height = Math.max(n.height, height(right1.right)) + 1;
 		return right1;
+	}
+	
+	/**
+	 * @Description: LR造成的不平衡，需要两次旋转。
+	 * @param n
+	 * @return
+	 */
+	private AVLNode doubleRotateLR(AVLNode n){
+		n.left = singleRotateLeft(n.left);
+		return singleRotateRight(n);
+	}
+	
+	/**
+	 * @Description: RL造成的不平衡，两次旋转。
+	 * @param n
+	 * @return
+	 */
+	private AVLNode doubleRotateRL(AVLNode n){
+		n.right = singleRotateRight(n.right);
+		return singleRotateLeft(n);
+	}
+	
+	public void insert(V v){
+		if(null == v){
+			throw new RuntimeException("Cannot insert null to AVLTree");
+		}
+		root = insert(v, root);
+	}
+	
+	/**
+	 * @Description: 向AVLTree中加入元素。
+	 * @param v
+	 */
+	private AVLNode insert(V v, AVLNode node){
+		//当前节点已经为空，新建结点。
+		if(null == node){
+			node = new AVLNode(v);
+		}else if (v.compareTo(node.v) < 0) {
+			//L
+			node.left = insert(v, node.left);
+			//如果造成了不平衡
+			if(height(node.left) - height(node.right) == 2){
+				//判断是LL还是LR
+				if(v.compareTo(node.left.v) < 0){
+					//LL
+					node = singleRotateRight(node);
+				}else{
+					//LR
+					node = doubleRotateLR(node);
+				}
+			}
+		}else if(v.compareTo(node.v) > 0){
+			//R
+			node.right = insert(v, node.right);
+			//判断是否需要旋转
+			if(height(node.right) - height(node.left) == 2){
+				//判断LR/RR
+				if(v.compareTo(node.right.v) > 0){
+					//RR
+					node = singleRotateLeft(node);
+				}else{
+					//RL
+					node = doubleRotateRL(node);
+				}
+			}
+		}else{
+			//如果要插入的数据和当前遍历到数据一致，do nothing
+			;
+		}
+		
+		node.height = Math.max(height(node.left), height(node.right)) + 1;
+		return node;
+	}
+	
+	public static void main(String[] args) {
+		BalancedBinaryTree<Integer> avlTree = new BalancedBinaryTree<>();
+		avlTree.insert(5);
+		System.out.println("insert :" + 5);
+		System.out.println(avlTree.root.v);
+		System.out.println(avlTree.height(avlTree.root));
+		System.out.println("-------------------------------------");
+		avlTree.insert(3);
+		System.out.println("insert :" + 3);
+		System.out.println(avlTree.root.v);
+		System.out.println(avlTree.height(avlTree.root));
+		System.out.println("-------------------------------------");
+		avlTree.insert(4);
+		System.out.println("insert :" + 4);
+		System.out.println(avlTree.root.v);
+		System.out.println(avlTree.height(avlTree.root));
+		System.out.println("-------------------------------------");
+		avlTree.insert(6);
+		System.out.println("insert :" + 6);
+		System.out.println(avlTree.root.v);
+		System.out.println(avlTree.height(avlTree.root));
+		System.out.println("-------------------------------------");
+		avlTree.insert(7);
+		System.out.println("insert :" + 7);
+		System.out.println(avlTree.root.v);
+		System.out.println(avlTree.height(avlTree.root));
+		System.out.println("-------------------------------------");
 	}
 }
